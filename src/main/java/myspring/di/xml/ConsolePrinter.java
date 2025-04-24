ea65694a60eb14e11070a4f06854cf9e95a549c9
nahye <knh8783@gmail.com>
2025-04-24 16:35:35 +0900

    add string bean

--- src/main/java/myspring/di/xml/ConsolePrinter.java
+++ src/main/java/myspring/di/xml/ConsolePrinter.java
@@ -1,6 +1,11 @@
 package myspring.di.xml;
 
 public class ConsolePrinter implements Printer {
+	
+	public ConsolePrinter() {
+		System.out.println(this.getClass().getName() + "기본 생성자 호출됨 !");
+	}
+	
 	public void print(String message) {
 		System.out.println(message);
 	}
