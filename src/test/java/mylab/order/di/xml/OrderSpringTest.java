842979ba9cdf22fd4b6c6dfa8573c2ee01725748
nahye <knh8783@gmail.com>
2025-04-25 23:50:08 +0900

    clear hw1-1 order classes

---
+++ src/test/java/mylab/order/di/xml/OrderSpringTest.java
@@ -0,0 +1,42 @@
+package mylab.order.di.xml;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import javax.annotation.Resource;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(locations = "classpath:mylab-order-di.xml")
+public class OrderSpringTest {
+	
+	@Resource(name = "shoppingCart")
+	ShoppingCart sc;
+	
+	@Resource(name = "orderService")
+	OrderService os;
+	
+	@Test
+	void testshoppingcart() {
+		assertNotNull(sc);
+		assertEquals(2, sc.getProducts().size());
+		assertEquals("product1", sc.getProducts().get(0).getName());
+		assertEquals("product2", sc.getProducts().get(1).getName());
+
+	}
+	
+	@Test
+	void testorderservice() {
+		assertNotNull(os);
+		assertEquals(sc, os.getShoppingCart());
+		assertEquals(2, os.getShoppingCart().getProducts().size());
+		assertEquals(6000, os.calculateOrderTotal());
+	}
+	
+
+}
