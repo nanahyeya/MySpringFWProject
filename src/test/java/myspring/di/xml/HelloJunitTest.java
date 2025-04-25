68e56374691f68649bab505c37835830cad2d5e8
nahye <knh8783@gmail.com>
2025-04-25 10:44:29 +0900

    add spring test

--- src/test/java/myspring/di/xml/HelloJunitTest.java
+++ src/test/java/myspring/di/xml/HelloJunitTest.java
@@ -1,24 +1,49 @@
 package myspring.di.xml;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
+
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.GenericXmlApplicationContext;
 
 // Assertions가 가진 모든 static 메서드를 import 하겠다 !!
 import static org.junit.jupiter.api.Assertions.*;
 
+
 public class HelloJunitTest {
+	
+	ApplicationContext context;
+	
+	@BeforeEach
+	void container() {
+		// 1. container 객체 생성
+		context = new GenericXmlApplicationContext("classpath:hello-di.xml");
+		
+		
+	}
+	
 	@Test
 	void helloBean() {
 //		System.out.println("Hello Junit !");
-		// 1. container 객체 생성
-		ApplicationContext context = new GenericXmlApplicationContext("classpath:hello-di.xml");
 		// 2. container 객체가 생성한 spring bean을 요청하기
 		Hello helloById =  (Hello)context.getBean("hello");
 		Hello helloByType = context.getBean("hello", Hello.class);
+		
 		// Spring Bean 의 주소 비교
 		System.out.println(helloById == helloByType);
-		assertSame(helloById, helloByType);
+		assertSame(helloById, helloByType); //주소 비교
+		//값비교
+		assertEquals("Hello 스프링", helloById.sayHello());
+		
+		helloById.print();
+		
+		// Contatiner 객체가 생성한 StringPrinter 스프링빈 요청하기
+		// <property name="printer" ref="strPrinter"/> 이 설정을 테스트 한 것임
+		Printer printer = context.getBean("strPrinter", Printer.class);
+		// toString 요청
+		assertEquals("Hello 스프링", printer.toString());
+
+		
 		
 	}
 }
---
+++ src/test/java/myspring/di/xml/HelloSpringTest.java
@@ -0,0 +1,31 @@
+package myspring.di.xml;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+// SpringExtension 클래스는 ApplicationContext(컨테이너) 객체를 생성하는 역할을 함
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(locations = "classpath:hello-di.xml")
+public class HelloSpringTest {
+	@Autowired
+	Hello hello;
+	
+	@Autowired
+	Printer printer;
+	
+	
+	@Test
+	void helloSpringBean() {
+		assertEquals("Hello 스프링", hello.sayHello());
+		hello.print();
+		assertEquals("Hello 스프링", printer.toString());
+
+		
+	}
+	
+}
