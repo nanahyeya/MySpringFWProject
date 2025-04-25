cbf24684bffe772622e844e42d851b37826fc45a
nahye <knh8783@gmail.com>
2025-04-25 16:04:05 +0900

    modify student package and add helloBean

---
+++ src/main/java/myspring/di/annot/ConsolePrinterBean.java
@@ -0,0 +1,16 @@
+package myspring.di.annot;
+
+import org.springframework.stereotype.Component;
+
+@Component("consolePrinter")
+
+public class ConsolePrinterBean implements PrinterBean {
+	
+	public ConsolePrinterBean() {
+		System.out.println(this.getClass().getName() + "기본 생성자 호출됨 !");
+	}
+	
+	public void print(String message) {
+		System.out.println(message);
+	}
+}
