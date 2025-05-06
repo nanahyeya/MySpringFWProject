be9acb9eedac4812c905b48384f7ee0f17e51b4e
nahye <knh8783@gmail.com>
2025-05-06 22:57:25 +0900

    add 0428 config package and @configuration

---
+++ src/main/java/myspring/di/annot/config/HelloBeanConfig.java
@@ -0,0 +1,10 @@
+package myspring.di.annot.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = {""})
+public class HelloBeanConfig {
+
+}
