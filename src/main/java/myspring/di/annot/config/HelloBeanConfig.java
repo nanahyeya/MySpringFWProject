032d53010e88a7a3c97196382c6d65528bc0545b
nahye <knh8783@gmail.com>
2025-05-06 22:59:45 +0900

    edit config

--- src/main/java/myspring/di/annot/config/HelloBeanConfig.java
+++ src/main/java/myspring/di/annot/config/HelloBeanConfig.java
@@ -2,9 +2,11 @@ package myspring.di.annot.config;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
 @Configuration
-@ComponentScan(basePackages = {""})
+@ComponentScan(basePackages = {"myspring.di.annot"})
+@PropertySource(value = "classpath:values.properties")
 public class HelloBeanConfig {
 
 }
